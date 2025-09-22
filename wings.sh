5a1a10ae4f30038e15da37f77bb32706410977eb
LakshyaTheMinecrafter <lakshyakatv@gmail.com>
2025-09-22 15:48:15 +0530

    Update wings.sh

--- wings.sh
+++ wings.sh
@@ -42,9 +42,7 @@ DAEMON_JSON="/etc/docker/daemon.json"
 # Create or overwrite the file with the desired content
 sudo tee "$DAEMON_JSON" > /dev/null <<'EOF'
 {
-    "iptables": false
-}
-{
+  "iptables": false,
   "dns": ["1.1.1.1", "8.8.8.8"]
 }
 EOF
