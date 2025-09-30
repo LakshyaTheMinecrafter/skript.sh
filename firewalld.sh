db758b60e15a329b3ab824379df5a0e29dea7ae1
LakshyaTheMinecrafter <lakshyakatv@gmail.com>
2025-09-30 09:21:57 +0530

    Update firewalld.sh

--- firewalld.sh
+++ firewalld.sh
@@ -21,6 +21,7 @@ sudo firewall-cmd --permanent --add-port=56423/tcp
 sudo firewall-cmd --permanent --add-port=8080/tcp
 sudo firewall-cmd --permanent --add-port=25565-25599/tcp
 sudo firewall-cmd --permanent --add-port=19132-19199/tcp
+sudo firewall-cmd --permanent --add-port=3306/tcp
 # UDP ports
 sudo firewall-cmd --permanent --add-port=8080/udp
 sudo firewall-cmd --permanent --add-port=25565-25599/udp
@@ -30,5 +31,5 @@ sudo firewall-cmd --permanent --add-port=19132-19199/udp
 sudo firewall-cmd --reload
 
 echo "✅ Firewalld setup complete!"
-echo "Allowed TCP: 80, 443, 2022, 5657, 56423, 8080, 25565-25599, 19132-19199"
+echo "Allowed TCP: 80, 443, 3306, 2022, 5657, 56423, 8080, 25565-25599, 19132-19199"
 echo "Allowed UDP: 8080, 25565-25599, 19132-19199"
