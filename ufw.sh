df53db0dd0663deccf309367355686cdf54c0ac5
LakshyaTheMinecrafter <lakshyakatv@gmail.com>
2025-11-15 22:43:22 +0530

    Update ufw.sh

--- ufw.sh
+++ ufw.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-
 set -e
 
+# Enable UFW without interaction
 sudo ufw enable
 
 # TCP ports
 for port in 80 443 2022 5657 56423 8080 25565:25599 19132:19199; do
     sudo ufw allow ${port}/tcp
 done
+
 # UDP ports
-for port in 8080 25565:25599 19132:19199; do
+for port in 80 443 2022 5657 56423 8080 25565:25599 19132:19199; do
     sudo ufw allow ${port}/udp
 done
+
 sudo ufw reload
 
 echo "✅ UFW setup complete!"
